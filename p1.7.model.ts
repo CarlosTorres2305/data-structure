e694dda5e45335c4c39e7e7e3cea4177e16976bf
carlostorres2305 <carlosadm15@hotmail.com>
2024-03-14 13:46:48 -0300

    exercicio p1.7

---
+++ p1.7.model.ts
@@ -0,0 +1,30 @@
+import * as readlineSync from 'readline-sync';
+export function menu(): void {
+    let op: boolean = true;
+    while (op == true) {
+        console.log('-----Menu-----');
+        console.log('1 - Dúvidas');
+        console.log('2 - Reclamações');
+        console.log('3 - Sair');
+        let userInput = parseInt(readlineSync.question("opcao: "));
+        switch (userInput) {
+            case 1: {
+                console.log("Suas dúvidas devem ser encaminhadas para o email: duvidas@email.com");
+                break;
+            }
+            case 2: {
+                let recl = readlineSync.question("Escreva sua reclamacao: ");
+                break;
+            }
+            case 3: {
+                op = false;
+                break;
+            }
+            default: {
+                console.log('opção inválida');
+            }
+        }
+    }
+
+}
+
---
+++ p1.7.view.ts
@@ -0,0 +1,5 @@
+import {menu} from './p1.7.model';
+//npm install --save-dev @types/readline-sync
+//npm install readline-sync
+
+menu();
